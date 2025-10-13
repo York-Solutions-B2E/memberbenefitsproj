321154551fed9576b4bb2add42ff3f6894a6c8ff
leng03 <102699960+leng03@users.noreply.github.com>
2025-10-13 11:24:18 -0500

    init setup

---
+++ memberbenefitsprojbe/src/main/java/com/yorksolutions/memberbenefitsprojbe/MemberbenefitsprojbeApplication.java
@@ -0,0 +1,13 @@
+package com.yorksolutions.memberbenefitsprojbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemberbenefitsprojbeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemberbenefitsprojbeApplication.class, args);
+    }
+
+}
---
+++ memberbenefitsprojbe/src/test/java/com/yorksolutions/memberbenefitsprojbe/MemberbenefitsprojbeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yorksolutions.memberbenefitsprojbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemberbenefitsprojbeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
